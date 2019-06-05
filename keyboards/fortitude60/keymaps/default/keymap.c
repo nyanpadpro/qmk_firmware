dd71c252dc3851d585ffc459120324e751a6c180
小川 裕太 <ogawa_yuuta@magus-inc.co.jp>
2019-06-05 12:44:11 +0900

    mod

--- keyboards/fortitude60/keymaps/default/keymap.c
+++ keyboards/fortitude60/keymaps/default/keymap.c
@@ -1,5 +1,5 @@
 #include QMK_KEYBOARD_H
-
+#include "keymap_jp.h"
 extern keymap_config_t keymap_config;
 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
@@ -34,11 +34,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|             |------+------+------+------+------+------|
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |             |   Y  |   U  |   I  |   O  |   P  | Bksp |
  * |------+------+------+------+------+------|             |------+------+------+------+------+------|
- * | Esc  |   A  |   S  |   D  |   F  |   G  |             |   H  |   J  |   K  |   L  |   ;  |  "   |
+ * | Esc  |   A  |   S  |   D  |   F  |   G  |             |   H  |   J  |   K  |   L  |   ;  |  :   |
  * |      |      |      |      |      |      |             |      |      |      |      |   +  |  *   |
  * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   [  |   ]  |   N  |   M  |   ,  |   .  |   /  |Enter |
- * |      |      |      |      |      |      |   `  |   {  |      |      |   <  |   >  |   ?  |      |
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   [  |   ]  |   N  |   M  |   ,  |   .  |   /  |Shift |
+ * |      |      |      |      |      |      |   {  |   }  |      |      |   <  |   >  |   ?  |      |
  * `-------------+------+------+------+------+------+------+------+------+------+------+-------------'
  *               |  GUI |  Alt |Lower |Space | Ctrl |Enter |Space |Raise |  Alt |  GUI |
  *               `---------------------------------------------------------------------'
@@ -46,8 +46,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT( \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-  KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC, KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
+  KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                   KC_H,    KC_J,    KC_K,    KC_L,    JP_SCLN, JP_COLN, \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, JP_LBRC, JP_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT , \
                  KC_LGUI, KC_LALT,   LOWER,  KC_SPC, KC_LCTL,  KC_ENT, KC_SPC, RAISE,    KC_RALT, KC_RGUI\
 ),
 
@@ -95,22 +95,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Lower
  * ,-----------------------------------------.             ,-----------------------------------------.
- * |   ~  |   !  |   @  |   #  |   $  |   %  |             |   ^  |   &  |   *  |   (  |   )  |      |
+ * |   ~  |   !  |   "  |   #  |   $  |   %  |             |   +  |   -  |   *  |   /  |   =  | yen  |
  * |------+------+------+------+------+------|             |------+------+------+------+------+------|
- * |   ~  |   !  |   "  |   #  |   $  |   %  |             |   ?  |   '  |   _  |  ¥   |   |  | Del  |
+ * |      |      |      |      |      |      |             |   ^  |   _  |   |  |      |   @  | Bks  |
  * |------+------+------+------+------+------|             |------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |             |  F6  |   =  |   +  |   `  |   {  |   }  |
+ * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |             |  F6  |      |      |      |      |      |
  * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |      |      |  F12 |ISO ~ |ISO | |      |   /  |      |
+ * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  英  |かな   |  F12 |      |      |      |      |      |
  * `-------------+------+------+------+------+------+------+------+------+------+------+-------------'
  *               |      |      |      |      |      |      |      |      |      |      |
  *               `---------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT( \
-  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, \
-  KC_TILD, KC_EXLM, KC_QUOT, KC_HASH, KC_DLR,  KC_PERC,                   KC_QUES, KC_QUOT, KC_UNDS, KC_JYEN, KC_PIPE, KC_DEL,  \
-  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_EQL, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______, _______, KC_F12,S(KC_NUHS),S(KC_NUBS),_______, _______, _______,\
+  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   JP_PLUS, JP_MINS, JP_ASTR, JP_SLSH, JP_EQL, JP_YEN, \
+  _______, _______, _______, _______, _______,  _______,                   JP_CIRC, JP_UNDS, JP_PIPE, _______, JP_AT, KC_BSPC,  \
+  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   _______, _______, _______, _______, _______, \
+  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  JP_MEISU, JP_MKANA, KC_F12,_______,_______,_______, _______, _______,\
                     _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______\
 ),
 
